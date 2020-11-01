a5b140b3546743320810bb3cfd0fffc5f1c5d35c
Guilherme Vaz <62060346+GuilhermeVazz@users.noreply.github.com>
2020-11-01 10:52:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,54 +3,38 @@ var selectPerson
 function gerarTexto() {
 
     selectPerson = document.getElementById("people").value
-        // for (var i = 0; i < personagens.length; i++) {
-        //     if (selectPerson == personagens[i].name) {
-        //         document.getElementById("name").innerHTML = personagens[i].name
-        //         document.getElementById("userName").innerHTML = personagens[i].userName
-        //         document.getElementById("showM").value = personagens[i].frase
-        //         document.getElementById("picUser").src = personagens[i].url
-        //     }
-        // }
-        personagens.forEach(p => {
-            if (selectPerson == p.name) {
-                        document.getElementById("name").innerHTML = p.name
-                        document.getElementById("userName").innerHTML = p.userName
-                        document.getElementById("showM").value = p.frase
-                        document.getElementById("picUser").src = p.url
-                    }
-        })
-}
+    // for (var i = 0; i < personagens.length; i++) {
+    //     if (selectPerson == personagens[i].name) {
+    //         document.getElementById("name").innerHTML = personagens[i].name
+    //         document.getElementById("userName").innerHTML = personagens[i].userName
+    //         document.getElementById("showM").value = personagens[i].frase
+    //         document.getElementById("picUser").src = personagens[i].url
+    //     }
+    // }
+    personagens.forEach(p => {
+        if (selectPerson == p.name) {
+            document.getElementById("name").innerHTML = p.name
+            document.getElementById("userName").innerHTML = p.userName
+            document.getElementById("showM").value = p.frase
 
-var personagens = [{
+            if (p.url == undefined) {
+                p.url = "./src/images/avatarPadrao.png"
+            } else {
+                document.getElementById("picUser").src = p.url
+            }
+        }
+    })
 
-    name: "Guilherme Vaz",
-    "userName": "@gVazz",
-    "frase": "Segue noixxxx ;) "
-}
-    ,
-{
-    "name": "Darth Vader",
-    "userName": "@LordVader",
-    "frase": "Eu sou seu PAI!!!!!"
-}
-    ,
-{
-    "name": "Chaves",
-    "userName": "@chavito",
-    "frase": "Issu, issu, issu..."
-}
-    ,
-{
-    "name": "Galvão Bueno",
-    "userName": "@falaMuitoGalvão",
-    "frase": "É tetraaaaaaaaaaa"
-}
-    ,
-{
-    "name": "Yoda",
-    "userName": "@yodaMestre",
-    "frase": "Que a Força esteja com você! ",
-    "url": "./src/images/yoda.png"
-}
 
-]
+    //data e hora
+    const zeroFill = n => {
+        return ('0' + n).slice(-2);
+    }
+    // Pega o horário atual
+    const now = new Date();
+    // Formata a data conforme dd/mm/aaaa hh:ii:ss
+    const dataHora = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear() + ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+    // Exibe na tela usando a div#data-hora
+    document.getElementById('dataHora').innerHTML = dataHora;
+
+}
