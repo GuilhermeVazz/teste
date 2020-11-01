4116eb5ed1d827c3268dc2c51304a7796568e0d9
Guilherme Vaz <62060346+GuilhermeVazz@users.noreply.github.com>
2020-11-01 10:53:14 -0300

    Update and rename teste.json to person.js

---
+++ person.js
@@ -0,0 +1,44 @@
+var personagens = [{
+
+    "name": "Guilherme Vaz",
+    "userName": "@gVazz",
+    "frase": "Segue noixxxx ;) ",
+    "url" : "./src/images/avatarPadrao.png"
+}
+    ,
+{
+    "name": "Darth Vader",
+    "userName": "@LordVader",
+    "frase": "Eu sou seu PAI!!!!!",
+    "url": "./src/images/darthVadepic.jpg"
+}
+    ,
+{
+    "name": "Chaves",
+    "userName": "@chavito",
+    "frase": "Issu, issu, issu...",
+    "url": "./src/images/chaves.jpeg"
+}
+    ,
+{
+    "name": "Galvão Bueno",
+    "userName": "@falaMuitoGalvão",
+    "frase": "É tetraaaaaaaaaaa",
+    "url": "./src/images/Galvao.jpg"
+}
+    ,
+{
+    "name": "Yoda",
+    "userName": "@yodaMestre",
+    "frase": "Que a Força esteja com você! ",
+    "url": "./src/images/yoda.png"
+}
+    ,
+{
+    "name": "Davi",
+    "userName": "@davikeps",
+    "frase": "Salve salve"
+}
+
+
+]
